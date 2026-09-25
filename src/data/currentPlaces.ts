c0a70f5d6e7574dd3825c8c5c50c868b03d09e98
Mikky1213 <mitsuki981213@gmail.com>
2026-09-25 18:27:19 +0900

    Remove mistaken 4494 current-place registration

--- src/data/currentPlaces.ts
+++ src/data/currentPlaces.ts
@@ -3,7 +3,6 @@ import { meisho7292 } from './meisho/7292'
 import { meisho4499 } from './meisho/4499'
 import { meisho4500 } from './meisho/4500'
 import { meisho4501 } from './meisho/4501'
-import { meisho4494 } from './meisho/4494'
 
 export type CurrentDocument = {
   title: string
@@ -49,7 +48,6 @@ export type CurrentPlace = {
 }
 
 export const currentPlaces: Record<number, CurrentPlace> = {
-  4494: meisho4494,
   4498: meisho4498,
   7292: meisho7292,
   4499: meisho4499,
