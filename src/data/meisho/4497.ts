e27d280e791dccfa767122c88548041e238e1674
Mikky1213 <mitsuki981213@gmail.com>
2026-09-26 21:24:32 +0900

    Add historical documents for Honnoji

--- src/data/meisho/4497.ts
+++ src/data/meisho/4497.ts
@@ -1,11 +1,24 @@
 import type { CurrentPlace } from '../currentPlaces'
 
 export const meisho4497: CurrentPlace = {
-  currentName: "妙永山本納寺",
+  currentName: '妙永山本納寺',
 
   address: '',
 
   description: '',
 
   photos: [],
+
+  documents: [
+    {
+      title: '『東京名所図会』本納寺',
+      text: '本納寺は鬼子母神堂の東道路の南に在り。妙永山と號す。法明寺に屬す。石門外に九老僧安置の石標を建つ。當寺は慶安三年庚寅實藏院日相上人の開基せし所なり。',
+      source: '『東京名所図会』',
+    },
+    {
+      title: '『新編武蔵風土記稿』本納寺',
+      text: '法明寺末。下二ヶ寺同し。妙永山ト號ス。本尊三寶祖師。辰巳ニ三光天子九老僧ノ像ヲ置。開山實藏院日相。延寶六年五月二日寂。\n三十番神堂　元禄九年九月二世日程建立ス。',
+      source: '『新編武蔵風土記稿』',
+    },
+  ],
 }
